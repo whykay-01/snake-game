e7faca6d3523b57f32437368400baf058fada1c0
Yan <100032599+whykay-01@users.noreply.github.com>
2023-05-26 22:16:26 +0700

    chore: reintalled the pygame module

--- python-files/main.py
+++ python-files/main.py
@@ -1,23 +1,20 @@
 import pygame
-import random
-import time
-
-# initialize pygame
 pygame.init()
-
-# create a screen
-screen = pygame.display.set_mode((800, 600))
-
-# title and icon
+screen = pygame.display.set_mode((400,300))
 pygame.display.set_caption("Snake Game")
-icon = pygame.image.load("python-files/icon.png")
-pygame.display.set_icon(icon)
-
-# snake
-# TODO: have a look at this part: might be very tricky!
-snake_img = pygame.image.load("python-files/snake.png")
-snake_x = 370
-snake_y = 480
-snake_x_change = 0
-snake_y_change = 0
+# icon = pygame.image.load("python-files/icon.png")
+# pygame.display.set_icon(icon)
+ 
+blue=(0,0,255)
+red=(255,0,0)
+ 
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over=True
+    pygame.draw.rect(screen,blue,[200,150,10,10])
+    pygame.display.update()
+pygame.quit()
+quit()
 
