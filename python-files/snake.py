ef0a6fd37ebaa0df6cca4d39d8e3aa65122ce2eb
Yan <100032599+whykay-01@users.noreply.github.com>
2023-05-28 12:54:29 +0700

    feat: added structure to the Snake class

---
+++ python-files/snake.py
@@ -0,0 +1,19 @@
+class Snake:
+    def __init__(self) -> None:
+        pass
+
+    def move(self):
+        pass
+
+    def eat(self):
+        pass
+
+    def die(self):
+        pass
+
+    def grow(self):
+        pass
+
+    def draw(self):
+        pass
+
